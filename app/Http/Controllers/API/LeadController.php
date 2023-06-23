59e3c0793164447a0f9537893ca1fce9a4b3b1f8
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-23 14:15:08 +0200

    Add custom validation to email

--- app/Http/Controllers/API/LeadController.php
+++ app/Http/Controllers/API/LeadController.php
@@ -2,23 +2,38 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
-  public function store(Request $request)
-  {
-    //dd($request->all());
-
-    //Validate data
-    
-    //if validation failure
-
-    //save data
-
-    //send email
-
-    //return success response  
-  }
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $data = $request->all();
+        //Validate data
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+        //if validation failure
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+        //save data
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+        //send email
+        Mail::to('info@boolpress.com')->send(new NewLead($newLead));
+        //return success response
+        return response()->json([
+          'success' => true,
+      ]);
+    }
 }
